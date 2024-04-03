ef1849ed7e320e944bada06b820e96d61e438eab
Davyd Derkach <165403123+dderkach-dedicatted@users.noreply.github.com>
2024-04-03 15:56:42 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.6"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.79.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     =  var.rg_name
+  location = "East US"
+}
