b9c97e8e08c59d49ea26a1c137e5c64cc169a3bd
dderkach-dedicatted <pilotindream@gmail.com>
2024-04-04 16:29:18 +0300

    env-reosurces

---
+++ us_region/env-resources/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = ">= 1.6"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.79.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "groups" {
+  type = map(object({
+    rg_name = string
+    location = string
+  }))
+  default = {
+    "first-dderkach" = {
+      rg_name = "first-dderkach-env-rg"
+      location = "South Central US"
+    },
+    "second_dderkach" = {
+      rg_name = "second-dderkach-env-rg"
+      location = "East US"
+    }
+  }
+}
+
+module "env_rg" {
+  source = "../../modules/resource_group"
+  groups = var.groups
+
+}
